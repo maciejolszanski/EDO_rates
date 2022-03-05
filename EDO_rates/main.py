5f65a5b7f1bfa602b75cb7347678b03bfca47633
Maciej Olszański <olszanskimaciej@wp.pl>
2022-03-05 17:30:37 +0100

    main.py: add funcs to get and validate input data
    get_user_data get the dates from user and lets user to stop the process

--- EDO_rates/main.py
+++ EDO_rates/main.py
@@ -1,12 +1,20 @@
 import requests
+import datetime
 
 from bs4 import BeautifulSoup
 
 
-def get_edo_rate(exp_date):
+INFO = '''\nHello!
+This program will show you what was the rent of Polish EDO through time.
+Follow the instructions below to receive an answer.
+(If you would like to quit the program type 'q')
+'''
 
-    url = f'https://www.obligacjeskarbowe.pl/oferta-obligacji/obligacje-10-letnie-edo/edo{exp_date}/'
+def get_edo_rate(exp_date_string):
+    '''Web scraping for the price of the EDO with particular expiration date'''
 
+    url = f'''https://www.obligacjeskarbowe.pl/oferta-obligacji/obligacje-10-letnie-edo/edo{exp_date_string}/'''
+    print(url)
     edo_site = requests.get(url).text
     edo_html = BeautifulSoup(edo_site, 'html.parser')
 
@@ -15,4 +23,54 @@ def get_edo_rate(exp_date):
 
     return rate
 
-print(get_edo_rate('1231'))
+def date_to_string(date):
+    '''creates a string formated as mmyy from date e/g/ 0322 from 01.03.2022'''
+    pass
+    
+def get_user_dates():
+    '''Get the dates from the user'''
+    end = False
+
+    init_str = input("Pass an initial date as MM-YY:\n")
+    init_date, end = validate_input_data(init_str)
+    if end or init_date is None:
+        return None, None, end
+
+    fin_str = input("Pass a final date as MM-YY:\n")
+    fin_date, end = validate_input_data(fin_str)
+    if end or fin_date is None:
+        return None, None, end
+    
+    return init_date, fin_date, end
+
+def validate_input_data(date_str):
+    '''check if the input data are valid'''
+
+    end = False
+
+    try:
+        date = init_date = datetime.datetime.strptime(date_str, '%m-%y')
+    except ValueError:
+        if date_str == 'q':
+            end = True
+            return None, end
+        else:
+            print('\nThe format of the dates is incorrect, it should be MM-YY.')
+            print('For example: March 2022 should be passed as 03-22.\n')
+            print("Let's try again:\n")
+            date = None
+    
+    return date, end
+
+if __name__ == '__main__':
+
+    print(INFO)
+
+    while True:
+        # ask user about the initial and final date to check EDO rates
+        init_date, fin_date, end = get_user_dates()
+        if end:
+            break
+        
+
+
