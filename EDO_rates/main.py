61b22ead45eaf642144884ec83a96990313d9efc
Maciej Olszański <olszanskimaciej@wp.pl>
2022-03-05 18:15:54 +0100

    main.py: add validate dates
    The function checks whether the final date is later than initial
    and if both days are earlier than today

--- EDO_rates/main.py
+++ EDO_rates/main.py
@@ -1,3 +1,4 @@
+from logging import raiseExceptions
 import requests
 import datetime
 
@@ -26,7 +27,7 @@ def get_edo_rate(exp_date_string):
 def date_to_string(date):
     '''creates a string formated as mmyy from date e/g/ 0322 from 01.03.2022'''
     pass
-    
+
 def get_user_dates():
     '''Get the dates from the user'''
     end = False
@@ -41,6 +42,11 @@ def get_user_dates():
     if end or fin_date is None:
         return None, None, end
     
+    # if the input strings are correct we have to chech if the dates are ok
+    valid = validate_dates(init_date, fin_date)
+    if not valid:
+        end = True
+
     return init_date, fin_date, end
 
 def validate_input_data(date_str):
@@ -49,7 +55,7 @@ def validate_input_data(date_str):
     end = False
 
     try:
-        date = init_date = datetime.datetime.strptime(date_str, '%m-%y')
+        date = datetime.datetime.strptime(date_str, '%m-%y')
     except ValueError:
         if date_str == 'q':
             end = True
@@ -59,8 +65,51 @@ def validate_input_data(date_str):
             print('For example: March 2022 should be passed as 03-22.\n')
             print("Let's try again:\n")
             date = None
-    
+
     return date, end
+
+def validate_dates(begin, finish):
+    '''
+        validate if the initial and final dates are correct
+        e.g. the initial date should be earlier than the final date
+
+        When one of the test is not failes it appends False to the 'valid' list
+        If ther is any False in the list the function returns false
+    '''
+    valid = []
+    # is the initial date earlier than the final date?
+    delta = finish - begin
+    if delta > datetime.timedelta(days=0):
+        valid.append(True)
+    else:
+        valid.append(False)
+        print('The initial date is later than or equal to the finish date')
+        print("Let's try again:\n")
+    
+    # is the initial or final date earlier than or equal to today?
+    today = datetime.date.today()
+    delta_init = today - begin
+    delta_fin = today - finish
+
+    if delta_init >= datetime.timedelta(days=0):
+        valid.append(True)
+    else:
+        valid.append(False)
+        print(f'The initial date is later than today')
+        print("Let's try again:\n")
+
+    if delta_fin >= datetime.timedelta(days=0):
+        valid.append(True)
+    else:
+        valid.append(False)
+        print(f'The initial date is later than today')
+        print("Let's try again:\n")
+
+    if False in valid:
+        return False
+    else:
+        return True
+   
 
 if __name__ == '__main__':
 
@@ -71,6 +120,3 @@ if __name__ == '__main__':
         init_date, fin_date, end = get_user_dates()
         if end:
             break
-        
-
-
